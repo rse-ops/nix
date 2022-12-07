0ef90ff59da1ff6a58e084e0ea6c6f29fe2ca6f2
Vanessasaurus <814322+vsoch@users.noreply.github.com>
2022-12-07 22:19:23 +0000

    add flux-core build
    Signed-off-by: Vanessasaurus <814322+vsoch@users.noreply.github.com>

---
+++ pkgs/hpc/flux-core/default.nix
@@ -0,0 +1,53 @@
+{ lib
+, stdenv
+, fetchurl
+, pkgs
+, maintainers
+}:
+
+
+stdenv.mkDerivation rec {
+  pname = "flux-core";
+  version = "0.45.0";
+
+  src = fetchurl {
+    url = "https://github.com/flux-framework/flux-core/releases/download/v${version}/flux-core-${version}.tar.gz";
+    sha256 = "6550fe682c1686745e1d9c201daf18f9c57691468124565c9252d27823d2fe46";
+  };
+
+
+  nativeBuildInputs = [
+    pkgs.pkgconfig
+    pkgs.autoconf
+    pkgs.automake
+    pkgs.libtool
+  ];
+
+  buildInputs = [
+    pkgs.cudatoolkit
+    pkgs.libarchive
+    pkgs.ncurses
+    pkgs.zmqpp
+    pkgs.czmq
+    pkgs.hwloc
+    pkgs.lua
+    pkgs.luajitPackages.luaposix
+    pkgs.python3
+    pkgs.libuuid
+    pkgs.python310Packages.cffi
+    pkgs.python310Packages.pyyaml
+    pkgs.python310Packages.jsonschema
+    pkgs.python310Packages.docutils
+    pkgs.jansson
+    pkgs.lz4
+    pkgs.sqlite
+    pkgs.asciidoc
+  ];
+
+  meta = with lib; {
+    description = "A next-generation resource manager.";
+    homepage = "https://github.com/flux-framework/flux-core";
+    license = licenses.lgpl3;
+    maintainers = [ maintainers.vsoch ];
+  };
+}
